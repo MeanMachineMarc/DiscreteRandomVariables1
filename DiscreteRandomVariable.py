be37ecd6ba073747850618ed9c0980e807b47557
MeanMachineMarc <marcus9981@icloud.com>
2022-05-25 17:06:53 +0100

    Changed naming

--- DiscreteRandomVariable.py
+++ DiscreteRandomVariable.py
@@ -1,4 +1,4 @@
-import MachineLib as M
+import MachineLib
 
 #Let X be the discrete random variable: 'number of birdies, or better'.
 #X ~ B(n, p), where n is the number of trials, and p is the probability of a machine achieving a birdie or better.
@@ -10,20 +10,20 @@ def bin_P(tail, r, n, p):
     if tail == 'L': #P(X <= r)
        total_probability = (1 - p) ** n 
        for r in range (1, (r + 1)): #Sum of probabilities up to and including r
-           total_probability = total_probability + (M.nCr(n, r) * (p ** r) * (1 - p) ** (n - r))
+           total_probability = total_probability + (MachineLib.nCr(n, r) * (p ** r) * (1 - p) ** (n - r))
         
-       return M.round_xsf(total_probability, 4)
+       return MachineLib.round_xsf(total_probability, 4)
 
     elif tail == 'M': #P(X = r)
        if r > 0: #P(X = r), r > 0
-          return M.round_xsf((M.nCr(n,r) * (p ** r) * (1 - p) ** (n - r)), 4)
+          return MachineLib.round_xsf((MachineLib.nCr(n,r) * (p ** r) * (1 - p) ** (n - r)), 4)
        
        else: #P(X = 0)
-          return M.round_xsf(((1 - p) ** n), 4)
+          return MachineLib.round_xsf(((1 - p) ** n), 4)
 
     else: #P(X >= r)
        if r > 0: #P(X >= r), r > 0
-          return M.round_xsf((1 - bin_P('L', (r - 1), n, p)), 4)
+          return MachineLib.round_xsf((1 - bin_P('L', (r - 1), n, p)), 4)
        
        else: #P(X >= 0)
           return 1

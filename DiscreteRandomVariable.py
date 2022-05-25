236a6e654f7d53edf1df76da86e8e84955acf2d6
MeanMachineMarc <marcus9981@icloud.com>
2022-05-25 16:35:23 +0100

    Nearly completed

--- DiscreteRandomVariable.py
+++ DiscreteRandomVariable.py
@@ -2,7 +2,24 @@ import MachineLib as M
 
 #Let X be the discrete random variable: 'number of birdies, or better'.
 #X ~ B(n, p), where n is the number of trials, and p is the probability of a birdie or better.
-def probability(tail, r, n, p):
-    if tail == 'L': 
-       return (nCr(n, r) * (p ** r) * (1 - p) ** (n - r))
-    
+def bin_P(tail, r, n, p):
+    r = int(r)
+    n = int(n)
+    p = float(p)
+
+    if tail == 'L': #P(X <= r)
+       total_probability = (1 - p) ** n 
+       for r in range (1, (r + 1)): #Sum of probabilities up to and including r
+           total_probability = total_probability + (M.nCr(n, r) * (p ** r) * (1 - p) ** (n - r))
+        
+       return M.round_xsf(total_probability, 4)
+
+    elif tail == 'M': #P(X = r)
+       if r > 0: #P(X = r), r > 0
+          return M.round_xsf((M.nCr(n,r) * (p ** r) * (1 - p) ** (n - r)), 4)
+       
+       else: #P(X = 0)
+          return M.round_xsf(((1 - p) ** n), 4)
+
+    else: #P(X >= r)
+       return M.round_xsf((1 - bin_P('L', (r - 1), n, p)), 4)

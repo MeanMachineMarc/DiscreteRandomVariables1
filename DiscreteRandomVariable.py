272c6a4805b1f1bd14187da3bed03abea2dce6cd
MeanMachineMarc <marcus9981@icloud.com>
2022-05-25 15:25:34 +0100

    .

--- DiscreteRandomVariable.py
+++ DiscreteRandomVariable.py
@@ -2,7 +2,7 @@ import MachineLib as M
 
 #Let X be the discrete random variable: 'number of birdies, or better'.
 #X ~ B(n, p), where n is the number of trials, and p is the probability of a birdie or better.
-def probability(tail, n, p):
+def probability(tail, r, n, p):
     if tail == 'L': 
-       return (nCr * (p ** r) * (1 - p) ** (n - r))
+       return (nCr(n, r) * (p ** r) * (1 - p) ** (n - r))
     
